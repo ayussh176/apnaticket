c974e7a0c415fa7c8bb101826606383e5fcbcdc7
ayussh176 <ayushmalik852@gmail.com>
2026-01-14 19:33:57 +0530

    client side initiated

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center text-3xl font-bold text-blue-600">
+      ✅ APNATICKET is running with Tailwind!
+    </div>
+  );
+}
+
+export default App;
